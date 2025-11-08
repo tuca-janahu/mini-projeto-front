f30fd19f6eb810ba56ac2d442447f5a712712b46
tuca-janahu <arturjanahu@gmail.com>
2025-11-08 18:49:43 -0300

    feat: register link and vercel.json

--- frontend/src/pages/Login/LoginPage.tsx
+++ frontend/src/pages/Login/LoginPage.tsx
@@ -4,6 +4,7 @@ import { toast } from "react-toastify";
 import { login as apiLogin } from "../../lib/api";
 import { useNavigate } from "react-router-dom";
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Login() {
     const [email, setEmail] = useState("");
@@ -43,7 +44,7 @@ export default function Login() {
           </div>
           <div className="py-8">
             <button type="submit" className="mt-6 w-full bg-blue-600 text-white p-2 rounded cursor-pointer">Entrar</button>
-          <button type="button" onClick={() => window.location.href='/register'} className="mt-6 w-full bg-white text-blue-600 border border-blue-600 p-2 rounded cursor-pointer">Cadastrar</button>
+          <Link to="/register" className="mt-6 w-full bg-white text-blue-600 border border-blue-600 p-2 rounded cursor-pointer">Cadastrar</Link>
           </div>
 
         </form>
